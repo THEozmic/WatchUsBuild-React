b5ac89d7383737f92d68e5ceb6fe0cccad12c4d6
THEozmic <michaelozoemena2@gmail.com>
2017-06-29 18:47:13 +0100

    Separate classes into individual files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import jQuery from 'jquery';
-import CommentBox from 'components/comment-box';
+import CommentBox from './components/comment-box';
 
 jQuery(function() {
   ReactDOM.render(
